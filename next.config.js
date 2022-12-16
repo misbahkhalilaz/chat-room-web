6b00ddb2d8faa2d292ba921e30b903e3bd4a8ef3
Misbah Khalil <misbah.khalil.az@gmail.com>
2022-12-16 15:57:58 +0500

    feat: Next app setup with TS, babel, multi env, scss

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const dev = require('./environments/shared/dev.json');
+const prod = require('./environments/shared/prod.json');
+const stage = require('./environments/shared/stage.json');
+const local = require('./environments/shared/local.json');
+
+module.exports = () => ({
+    webpack: (config) => {
+        /* Next JS path aliases */
+        config.resolve.alias['root'] = path.join(__dirname);
+        config.resolve.alias['src'] = path.join(__dirname, 'src');
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                },
+            },
+        });
+
+        return config;
+    },
+    publicRuntimeConfig: {
+        env: {local, dev, stage, prod},
+    },
+});
