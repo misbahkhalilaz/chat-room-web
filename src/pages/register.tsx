024ea03f072a42d5deb246f46f9513f938608b18
Misbah Khalil <misbah.khalil.az@gmail.com>
2022-12-17 14:00:34 +0500

    feat: register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,61 @@
+import {NextPage} from 'next';
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import {useDispatch} from 'react-redux';
+import {InputField} from 'src/components/InputField/InputField';
+import {registerAction} from 'src/redux/auth/auth.action';
+
+const RegisterPage: NextPage = () => {
+    const dispatch = useDispatch();
+    const {
+        handleSubmit,
+        register,
+        formState: {errors},
+    } = useForm<RegisterUser>();
+
+    const requiredRule = {
+        value: true,
+        message: 'This field is required.',
+    };
+
+    return (
+        <form
+            className="form-card"
+            onSubmit={handleSubmit((formState) => {
+                dispatch(registerAction(formState));
+            })}>
+            <h1>Signup</h1>
+            <InputField
+                {...register('firstName', {required: requiredRule})}
+                label="First Name"
+                autoFocus={true}
+                isError={!!errors.firstName}
+                message={errors.firstName?.message}
+            />
+            <InputField
+                {...register('lastName', {required: requiredRule})}
+                label="Last Name"
+                autoFocus={true}
+                isError={!!errors.lastName}
+                message={errors.lastName?.message}
+            />
+            <InputField
+                {...register('userName', {required: requiredRule})}
+                label="Username"
+                autoFocus={true}
+                isError={!!errors.userName}
+                message={errors.userName?.message}
+            />
+            <InputField
+                {...register('password', {required: requiredRule})}
+                label="Password"
+                autoFocus={true}
+                isError={!!errors.password}
+                message={errors.password?.message}
+            />
+            <button type="submit">Register</button>
+        </form>
+    );
+};
+
+export default RegisterPage;
