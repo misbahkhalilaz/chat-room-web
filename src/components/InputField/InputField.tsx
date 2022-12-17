aa824138b25faccdec1dd3a8edbd9450d88a7658
Misbah Khalil <misbah.khalil.az@gmail.com>
2022-12-17 13:59:27 +0500

    feat: input field component

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames';
+import React, {forwardRef} from 'react';
+import {InputFieldProps} from 'src/types/input';
+
+export const InputField = forwardRef<HTMLInputElement, InputFieldProps>(
+    ({label, message, isError = false, disableLabelTransition = true, ...fieldProps}, ref) => {
+        const inputId = Math.random().toString();
+
+        return (
+            <div
+                className={classNames('input-group', {
+                    'input-group--error': isError,
+                })}>
+                <input {...fieldProps} ref={ref} id={inputId} />
+                <label
+                    htmlFor={inputId}
+                    className={classNames('input-group__label', {
+                        'input-group__label--focused': fieldProps.value || disableLabelTransition,
+                        'input-group__label--blured': !fieldProps.value || !disableLabelTransition,
+                    })}>
+                    {label}
+                </label>
+                {message && (
+                    <label htmlFor={inputId} className="input-group__message">
+                        {message}
+                    </label>
+                )}
+            </div>
+        );
+    },
+);
